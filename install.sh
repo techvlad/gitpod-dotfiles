61c030d591708cac444baf10c63491d3ea67d5c9
Vlad <13676043+techvlad@users.noreply.github.com>
2022-08-20 13:34:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 curl -sS https://starship.rs/install.sh | sh -s - --yes
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
+# Fig integration
+source <(curl -Ls fig.io/install)
+
 # Install packages
 apt install htop jq

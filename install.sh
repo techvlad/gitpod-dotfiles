caacdae35c111aa7bc98b3e7a69accb0046667bc
Vlad <13676043+techvlad@users.noreply.github.com>
2022-10-11 17:40:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ curl -sS https://starship.rs/install.sh | sh -s - --yes
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
 # Install packages
-sudo apt install htop jq --yes
+sudo apt install htop jq fzf --yes
 
 # Install bat
 wget https://github.com/sharkdp/bat/releases/download/v0.21.0/bat-musl_0.21.0_amd64.deb

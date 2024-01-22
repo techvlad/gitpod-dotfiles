a8754dc1cbed29377146b893f7b2797fd51046e2
Vlad Krokhin <3v1lh3x@gmail.com>
2024-01-22 17:45:41 +0200

    update

--- .bash_aliases
+++ .bash_aliases
@@ -1,1 +1,5 @@
-# alias cat='bat -pp --theme=gruvbox-dark'
+# Aliases
+alias refresh_backend="cd /workspace/shares/projects/backend; git pull origin dev --ff-only && pnpm i && pnpm db:update"
+
+# Add custom bin folder
+export PATH=$HOME/.local/bin:$PATH
--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@
 curl -sS https://starship.rs/install.sh | sh -s - --yes
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
-# Install packages
-sudo apt install htop jq fzf --yes
+# z, zi (zoxide)
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
-# Install bat
-wget https://github.com/sharkdp/bat/releases/download/v0.21.0/bat-musl_0.21.0_amd64.deb
-dpkg -i bat-musl_0.21.0_amd64.deb
-rm bat-musl_0.21.0_amd64.deb
+# Install packages
+sudo apt-get install htop fzf --yes
+
+# Lazydocker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash

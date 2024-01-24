b9539aa910fd57415764735f848f1a39a034baf7
Vlad Krokhin <3v1lh3x@gmail.com>
2024-01-24 15:45:57 +0200

    remove zi

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@
 curl -sS https://starship.rs/install.sh | sh -s - --yes
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
-# z, zi (zoxide)
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-
 # Install packages
 sudo apt-get install htop fzf --yes
 

3c7272f599e6157b6898ab75df2de1d1b635d550
Vlad Krokhin <3v1lh3x@gmail.com>
2024-01-26 16:41:34 +0200

    (neo)vim

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # Install fancy shell
 curl -sS https://starship.rs/install.sh | sh -s - --yes
@@ -10,3 +11,12 @@ sudo apt-get install htop fzf --yes
 
 # Lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
+# Nvim
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+chmod +x ./nvim-linux64/bin/nvim
+sudo ln -s "$(realpath ./nvim-linux64/bin/nvim)" /usr/bin/nvim
+
+# NvChad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim -Es
